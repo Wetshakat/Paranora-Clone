2ed52e40545e5faab01e2db405427c341908f24d
Wetshakat <ishakudyelshak.com>
2025-02-16 05:43:27 +0000

    paranora

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}", "./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        monst:["Montserrat", "serif"]
+      },
+    },
+  },
+  plugins: [],
+}
